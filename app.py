77a6f37c9a61fbe8fdff94838bb518914d546f37
Niclas Mølby <niclas@molby.net>
2018-11-12 16:05:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/users', methods=['GET'])
 def getUsers():
     return "All gut", 200
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    #application.debug = True
+    app.run()
