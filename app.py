57e4a96b3fcbc94a9ebac67a07aad6bb3c8c3e23
Niclas Mølby <niclas@molby.net>
2018-11-12 12:46:55 +0100

    renamed method

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/get-users', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def getUsers():
     return "All gut", 200
